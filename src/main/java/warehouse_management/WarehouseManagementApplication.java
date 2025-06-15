e18cd4b17cd2cf2132e0f9b293c8d8889c1cbe75
Staša Sekulić <stasa.sekulic.91@gmail.com>
2025-06-15 13:21:30 +0200

    Added EnableJpaAuditing

--- src/main/java/warehouse_management/WarehouseManagementApplication.java
+++ src/main/java/warehouse_management/WarehouseManagementApplication.java
@@ -2,8 +2,11 @@ package warehouse_management;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
+
 
 @SpringBootApplication
+@EnableJpaAuditing
 public class WarehouseManagementApplication {
 
 	public static void main(String[] args) {
